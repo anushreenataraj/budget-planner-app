4663a5d4f2cefb34530655803d8ebd92c8ba94af
deepikasai7 <ksdrao7@gmail.com>
2021-05-18 19:40:46 +0530

    add your mail id

--- budgetapp/__init__.py
+++ budgetapp/__init__.py
@@ -2,15 +2,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail, Message
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8eba05cf8f1242c3c05ca3318d8efd2e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOAD_FOLDER'] = r'C:\Users\sai\Desktop\bapp\budget-planner-app\budgetapp\upload'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = ''# add your mail id
+app.config['MAIL_PASSWORD'] = ''# add your password
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+mail = Mail(app)
 
 from budgetapp import routes
